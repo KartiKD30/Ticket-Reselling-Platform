f17c2adbe1353d8ab0bf5943f10d80c4456280d2
Kartik <kartik.deshmukh3099@gmail.com>
2026-04-08 10:01:25 +0530

    Delete backend directory

--- backend/models/Waitlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WaitlistSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-  email: { type: String, required: true },
-  notified: { type: Boolean, default: false }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Waitlist', WaitlistSchema);
